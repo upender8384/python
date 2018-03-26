ce243022022ae42009717606ea264653a2805c29
upender8384 <upender.devanalyst@gmail.com>
2018-03-26 19:51:35 +0530

    pyhton basics

---
+++ basicmath.py
@@ -0,0 +1,35 @@
+# basic math operations
+
+x=3
+y=5
+
+sum = x+y
+print "sum value is ",sum
+
+mul=x*y
+
+print 'mul value is:',mul
+
+div=x/y
+
+print ('div value is :',div)
+
+per=x%y
+
+print 'per value is ',per
+
+
+# by taking input from the panel and add two numbers
+
+num1 = input('enter first number: ')
+num2 = input('enter second number : ')
+
+#add two numbers
+sum1 = num1+num2
+
+# display the sum 
+print('the sum of {0} and {1} is{2} :'. format (num1,num2,sum1))
+
+
+
+
---
+++ printexample.py
@@ -0,0 +1,7 @@
+# basic program for print in pyhton 3 
+
+print ('This is the first program for test print');
+
+print ('we\'re going to mall ')
+
+print (int ('8') + 5)
